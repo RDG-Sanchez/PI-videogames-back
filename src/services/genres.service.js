bdf4268f48aea3123f12476bc925b42378951db8
“Rafael” <rafaelzhx@gmail.com>
2024-03-09 16:34:54 -0300

    finish back

--- src/services/genres.service.js
+++ src/services/genres.service.js
@@ -13,14 +13,21 @@ const { API_KEY } = process.env;
 
 const serv_getGenres = async () => {
   try {
-    // const response = await axios(
-    //   `http://api.rawg.io/api/genres?key=${API_KEY}`
-    // );
-    // const sendGenres = response.data.results.map((genre) => {
-    //   return { name: genre.name };
-    // });
-    // await Genre.bulkCreate(sendGenres);
-    return await Genre.findAll();
+    const genresDB = await Genre.findAll();
+    if (genresDB.length === 0) {
+      const response = await axios(
+        `http://api.rawg.io/api/genres?key=${API_KEY}`
+      );
+
+      const sendGenres = response.data.results.map((genre) => {
+        return { name: genre.name };
+      });
+
+      await Genre.bulkCreate(sendGenres);
+      return await Genre.findAll();
+    } else {
+      return await Genre.findAll();
+    }
   } catch (error) {
     throw new Error(error.message);
   }
